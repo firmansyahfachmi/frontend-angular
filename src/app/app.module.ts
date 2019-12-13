3cdf0a36d30750752c8898f67398acfc9bb57a8d
M Fachmi Firmansyah <firmansyahfachmi5@gmail.com>
2019-12-14 00:33:31 +0700

    update content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { VideosComponent } from './videos/videos.component';
+import { ActivityComponent } from './activity/activity.component';
+import { PeopleComponent } from './people/people.component';
+import { DocumentsComponent } from './documents/documents.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    NavigationComponent,
+    VideosComponent,
+    ActivityComponent,
+    PeopleComponent,
+    DocumentsComponent
   ],
   imports: [
     BrowserModule
